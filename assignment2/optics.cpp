c85b2409c8d029a332d40bb53f5a792ae50b2b4d
Nikhil Goyal <nikhil10sep@gmail.com>
2018-09-20 05:10:24 +0530

    optics chal gaya

--- assignment2/optics.cpp
+++ assignment2/optics.cpp
@@ -6,137 +6,139 @@
 namespace RT = RangeTree;
 using namespace std;
 
-// vector<int> optics(vector<vector<float> > &data, int tau, float eps);
-// void update(vector<int> &N, int p, float core_dist, set<pair<float, int> > &seeds, float eps, int tau);
+void optics(vector<vector<float> > &data, int tau, float eps);
+void update(vector<int> &N, int p, float core_dist, set<pair<float, int> > &seeds, float eps, int tau);
 
 vector<vector<float> > data;
 vector<bool> visited;
-vector<int> ordering, reach_dist;
+vector<int> ordering;
+vector<float> reach_dist;
 
 int main(int argc, char* argv[]) {
     string file = argv[1];
     int tau = atoi(argv[2]);
     float eps = atof(argv[3]);
     data = read_file(file);
-    // int n = data.size();
+    int n = data.size();
 
-    // vector<int> clusters = optics(data, tau, eps);
-    // ofstream fw("out_dbscan2.txt");
-    // for(int i = 0; i < n; i++) {
-    //     fw << clusters[i] << endl;
-    // }
+    optics(data, tau, eps);
+    ofstream fw("out_optics.txt");
+    for(int i = 0; i < n; i++) {
+        fw << reach_dist[ordering[i]]<< endl;
+    }
     return 0;
 }
 
-// vector<int> optics(vector<vector<float> > &data, int tau, float eps) {
-//     float eps2 = eps * eps;
-//     int n = data.size();
-//     int d = data[0].size();
+void optics(vector<vector<float> > &data, int tau, float eps) {
+    float eps2 = eps * eps;
+    int n = data.size();
+    int d = data[0].size();
 
-//     // Make range tree
-// 	vector<RT::Point<float,int> > points;
-// 	for(int i = 0; i < n; i++) {
-// 		points.push_back(RT::Point<float,int>(data[i], i));
-// 	}
+    // Make range tree
+	vector<RT::Point<float,int> > points;
+	for(int i = 0; i < n; i++) {
+		points.push_back(RT::Point<float,int>(data[i], i));
+	}
 
-// 	RT::RangeTree<float,int> rtree(points);
-// 	vector<bool> withLower(d, true), withUpper(d, true);
+	RT::RangeTree<float,int> rtree(points);
+	vector<bool> withLower(d, true), withUpper(d, true);
 
-//     visited = vector<bool>(n, false);
-//     reach_dist = vector<int>(n, -1);
-//     // int cluster_val = -1;
+    visited = vector<bool>(n, false);
+    reach_dist = vector<float>(n, -1.0);
+    // int cluster_val = -1;
 
-//     for(int i = 0; i < n; i++) {
-//         if(visited[i]) {
-//             continue;
-//         }
+    for(int i = 0; i < n; i++) {
+        if(visited[i]) {
+            continue;
+        }
 
-//         vector<float> lower(d), upper(d);
-//         for(int j = 0; j < d; j++) {
-//         	lower[j] = data[i][j] - eps;
-//         	upper[j] = data[i][j] + eps;
-//         }
-//         vector<RT::Point<float,int> > box = rtree.pointsInRange(lower, upper, withLower, withUpper);
+        vector<float> lower(d), upper(d);
+        for(int j = 0; j < d; j++) {
+        	lower[j] = data[i][j] - eps;
+        	upper[j] = data[i][j] + eps;
+        }
+        vector<RT::Point<float,int> > box = rtree.pointsInRange(lower, upper, withLower, withUpper);
 
-//         vector<int> N;
-//         vector<float> tbs; 
-//         for(int j = 0; j < box.size(); j++) {
-//             if(box[j].value() == i) {
-//             	continue;
-//             } else if(distance(data[i], box[j].asVector()) < eps2) {
-//             	N.push_back(box[j].value());
-//                 tbs.push_back(distance(data[i], box[j].asVector()));
-//             }
-//         }
+        vector<int> N;
+        vector<float> tbs; 
+        for(int j = 0; j < box.size(); j++) {
+            if(box[j].value() == i) {
+            	continue;
+            } else if(distance(data[i], box[j].asVector()) < eps2) {
+            	N.push_back(box[j].value());
+                tbs.push_back(distance(data[i], box[j].asVector()));
+            }
+        }
 
-//         visited[i] = true;
-//         ordering.push_back(i);
+        visited[i] = true;
+        ordering.push_back(i);
 
-//         sort(tbs.begin(), tbs.end());
+        sort(tbs.begin(), tbs.end());
 
-//         if(N.size() < tau) {
-//             continue;
-//         } 
+        if(N.size() < tau) {
+            continue;
+        } 
         
-//         float core_dist = tbs[tau - 1];
-//         set<pair<float, int> > seeds;
-//         update(N, i, core_dist, seeds, eps, tau);
+        float core_dist = tbs[tau - 1];
+        cout << core_dist << endl;
+        set<pair<float, int> > seeds;
+        update(N, i, core_dist, seeds, eps, tau);
 
-//         while(!seeds.empty()) {
-//             set<pair<float, int> > :: iterator it = seeds.begin();
-//             int q = it->second;
-//             seeds.erase(it);
+        while(!seeds.empty()) {
+            set<pair<float, int> > :: iterator it = seeds.begin();
+            int q = it->second;
+            seeds.erase(it);
 
-//             vector<float> lower(d), upper(d);
-//             for(int j = 0; j < d; j++) {
-//                 lower[j] = data[q][j] - eps;
-//                 upper[j] = data[q][j] + eps;
-//             }
-//             vector<RT::Point<float,int> > box = rtree.pointsInRange(lower, upper, withLower, withUpper);
+            vector<float> lower(d), upper(d);
+            for(int j = 0; j < d; j++) {
+                lower[j] = data[q][j] - eps;
+                upper[j] = data[q][j] + eps;
+            }
+            vector<RT::Point<float,int> > box = rtree.pointsInRange(lower, upper, withLower, withUpper);
 
-//             vector<int> Np;
-//             vector<float> tbsp; 
-//             for(int j = 0; j < box.size(); j++) {
-//                 if(box[j].value() == q) {
-//                     continue;
-//                 } else if(distance(data[q], box[j].asVector()) < eps2) {
-//                     Np.push_back(box[j].value());
-//                     tbsp.push_back(distance(data[q], box[j].asVector()));
-//                 }
-//             }
+            vector<int> Np;
+            vector<float> tbsp; 
+            for(int j = 0; j < box.size(); j++) {
+                if(box[j].value() == q) {
+                    continue;
+                } else if(distance(data[q], box[j].asVector()) < eps2) {
+                    Np.push_back(box[j].value());
+                    tbsp.push_back(distance(data[q], box[j].asVector()));
+                }
+            }
 
-//             visited[q] = true;
-//             ordering.push_back(q);
+            visited[q] = true;
+            ordering.push_back(q);
 
-//             sort(tbsp.begin(), tbsp.end());
+            sort(tbsp.begin(), tbsp.end());
 
-//             if(Np.size() >= tau) {
-//                 float core_distp = tbsp[tau - 1];
-//                 update(Np, q, core_distp, seeds, eps, tau);
-//             }
-//         }
-//     }
+            if(Np.size() >= tau) {
+                float core_distp = tbsp[tau - 1];
+                update(Np, q, core_distp, seeds, eps, tau);
+            }
+        }
+    }
 
-//     return vector<int>();
-// }
+    return;
+}
 
-// void update(vector<int> &N, int p, float core_dist, set<pair<float, int> > &seeds, float eps, int tau) {
-//     for(int i = 0; i < N.size(); i++) {
-//         int o = N[i];
-//         if(visited[o]) {
-//             continue;
-//         }
+void update(vector<int> &N, int p, float core_dist, set<pair<float, int> > &seeds, float eps, int tau) {
+    for(int i = 0; i < N.size(); i++) {
+        int o = N[i];
+        if(visited[o]) {
+            continue;
+        }
 
-//         float dist_p_o = distance(data[p], data[o]);
-//         float new_reach_dist = core_dist > dist_p_o ? core_dist : dist_p_o;
-//         if(reach_dist[o] == -1) {
-//             reach_dist[o] = new_reach_dist;
-//             seeds.insert(make_pair(new_reach_dist, o));
-//         } else if(new_reach_dist < reach_dist[o]) {
-//             seeds.erase(seeds.find(make_pair(reach_dist[o], o)));
-//             seeds.insert(make_pair(new_reach_dist, o));
-//             reach_dist[o] = new_reach_dist;
-//         }   
-//     }
-//     return;
-// }
+        float dist_p_o = distance(data[p], data[o]);
+        float new_reach_dist = core_dist > dist_p_o ? core_dist : dist_p_o;
+        if(reach_dist[o] == -1) {
+            reach_dist[o] = new_reach_dist;
+            seeds.insert(make_pair(new_reach_dist, o));
+        } else if(new_reach_dist < reach_dist[o]) {
+            seeds.erase(seeds.find(make_pair(reach_dist[o], o)));
+            seeds.insert(make_pair(new_reach_dist, o));
+            reach_dist[o] = new_reach_dist;
+        }   
+    }
+    return;
+}
