7b53ee5336b4e63bf3cffcb5758e9e30bd33e054
lovishmadaan <lovishmadaan124@gmail.com>
2018-09-21 17:06:04 +0530

    optics KD

--- assignment2/optics.cpp
+++ assignment2/optics.cpp
@@ -1,10 +1,12 @@
 #include"preprocess.h"
-#include"RangeTree.h"
+#include"nanoflann.hpp"
+
+#include"KDTreeVectorOfVectorsAdaptor.h"
 #include<cfloat>
 #include<set>
 
-namespace RT = RangeTree;
 using namespace std;
+using namespace nanoflann;
 
 void optics(vector<vector<float> > &data, int tau, float eps);
 void update(vector<int> &N, int p, float core_dist, set<pair<float, int> > &seeds, float eps, int tau);
@@ -42,13 +44,13 @@ void optics(vector<vector<float> > &data, int tau, float eps) {
     int d = data[0].size();
 
     // Make range tree
-	vector<RT::Point<float,int> > points;
-	for(int i = 0; i < n; i++) {
-		points.push_back(RT::Point<float,int>(data[i], i));
-	}
+	typedef KDTreeVectorOfVectorsAdaptor<vector<std::vector<float>>, float> kd_tree_t;
+    kd_tree_t kdtree(d, data, 20);
 
-	RT::RangeTree<float,int> rtree(points);
-	vector<bool> withLower(d, true), withUpper(d, true);
+    kdtree.index->buildIndex();
+
+    SearchParams params;
+    params.sorted = true;
 
     visited = vector<bool>(n, false);
     reach_dist = vector<float>(n, -1.0);
@@ -58,35 +60,26 @@ void optics(vector<vector<float> > &data, int tau, float eps) {
             continue;
         }
 
-        vector<float> lower(d), upper(d);
-        for(int j = 0; j < d; j++) {
-        	lower[j] = data[i][j] - eps;
-        	upper[j] = data[i][j] + eps;
-        }
-        vector<RT::Point<float,int> > box = rtree.pointsInRange(lower, upper, withLower, withUpper);
+        vector<pair<size_t, float> >  ret_matches;
+
+        const size_t nMatches = kdtree.index->radiusSearch(&data[i][0], eps, ret_matches, params);
 
         vector<int> N;
-        vector<float> tbs; 
-        for(int j = 0; j < box.size(); j++) {
-            if(box[j].value() == i) {
-            	continue;
-            } else if(distance(data[i], box[j].asVector()) < eps2) {
-            	N.push_back(box[j].value());
-                tbs.push_back(distance(data[i], box[j].asVector()));
+        for(int j = 0; j < ret_matches.size(); j++) {
+            if(ret_matches[j].first != i) {
+            	N.push_back(ret_matches[j].first);
             }
         }
 
         visited[i] = true;
         ordering.push_back(i);
 
-        sort(tbs.begin(), tbs.end());
-
         if(N.size() < tau) {
             continue;
         } 
         
-        float core_dist = tbs[tau - 1];
-        // cout << core_dist << endl;
+        float core_dist = ret_matches[tau - 1].second;
+        cout << i << " " << core_dist << endl;
         set<pair<float, int> > seeds;
         update(N, i, core_dist, seeds, eps, tau);
 
@@ -95,31 +88,22 @@ void optics(vector<vector<float> > &data, int tau, float eps) {
             int q = it->second;
             seeds.erase(it);
 
-            vector<float> lower(d), upper(d);
-            for(int j = 0; j < d; j++) {
-                lower[j] = data[q][j] - eps;
-                upper[j] = data[q][j] + eps;
-            }
-            vector<RT::Point<float,int> > box = rtree.pointsInRange(lower, upper, withLower, withUpper);
+            vector<pair<size_t, float> >  temp_box;
 
-            vector<int> Np;
-            vector<float> tbsp; 
-            for(int j = 0; j < box.size(); j++) {
-                if(box[j].value() == q) {
-                    continue;
-                } else if(distance(data[q], box[j].asVector()) < eps2) {
-                    Np.push_back(box[j].value());
-                    tbsp.push_back(distance(data[q], box[j].asVector()));
+            const size_t temp_count = kdtree.index->radiusSearch(&data[q][0], eps, temp_box, params);
+
+            vector<int> Np; 
+            for(int j = 0; j < temp_box.size(); j++) {
+                if(temp_box[j].first != q) {
+                    Np.push_back(temp_box[j].first);
                 }
             }
 
             visited[q] = true;
             ordering.push_back(q);
 
-            sort(tbsp.begin(), tbsp.end());
-
             if(Np.size() >= tau) {
-                float core_distp = tbsp[tau - 1];
+                float core_distp = temp_box[tau - 1].second;
                 update(Np, q, core_distp, seeds, eps, tau);
             }
         }
